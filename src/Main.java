efc6d9058f3de958fa3f453eaab416865f293106
AndreyKarabut <karabut.andrey2018@yandex.ru>
2022-02-19 10:50:11 +0600

    Task 14.7.1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Main extends Thread {
     static ArrayList<Client> arrayList;
+    static private AtomicInteger count = new AtomicInteger(0);
 
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
+    public static void main(String[] args) throws InterruptedException {
         arrayList = new ArrayList<>();
         createClients();
         processingClients();
-        long end = System.currentTimeMillis();
-        System.out.println("Time is: " + (end - start));
+        Thread.sleep(500);
+        System.out.println(arrayList.get(999).getDebt());
     }
-
 
 
     private static void processingClients() {
         int threats = arrayList.size() / 300;
         for (int i = 0; i < threats + 1; i++) {
             MyThread tr = new MyThread();
-            tr.setName(String.valueOf(i));
             tr.start();
         }
     }
@@ -31,36 +30,26 @@ public class Main extends Thread {
             Client client = new Client("name" + i, random.nextInt(40) + 18);
             arrayList.add(client);
         }
-        System.out.println();
     }
 
-    static class MyThread extends Thread{
+    static class MyThread extends Thread {
         @Override
         public void run() {
-            int threatNumber = Integer.parseInt(getName());
             Client copy;
-            Client client;
-            if (arrayList.size() / 300 == threatNumber) {
-                for (int i = threatNumber * 300; i < arrayList.size(); i++) {
-                    copy = arrayList.get(i);
-                    if (copy != null) {
-                        client = new Client(copy.getName(), copy.getAge());
-                        client.setTurnover(copy.getTurnover() + 100);
-                        client.setDebt((float) (copy.getDebt() + 1000.0));
-                        arrayList.set(i, client);
-                    }
-                }
-            } else {
-                for (int i = threatNumber * 300; i < (threatNumber + 1) * 300; i++) {
-                    copy = arrayList.get(i);
-                    if (copy != null) {
-                        client = new Client(copy.getName(), copy.getAge());
-                        client.setTurnover(copy.getTurnover() + 100);
-                        client.setDebt((float) (copy.getDebt() + 1000.0));
-                        arrayList.set(i, client);
-                    }
-                }
+            int i = count.getAndIncrement();
+            while (i < arrayList.size()) {
+                copy = arrayList.get(i);
+                changeClient(copy, i);
+                System.out.println("Client " + i + " changed");
+                i = count.getAndIncrement();
             }
         }
+    }
+
+    private static void changeClient(Client copy, int i) {
+        Client client = new Client(copy.getName(), copy.getAge());
+        client.setTurnover(copy.getTurnover() + 100);
+        client.setDebt((float) (copy.getDebt() + 1000.0));
+        arrayList.set(i, client);
     }
 }
